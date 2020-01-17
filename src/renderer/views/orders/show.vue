eead9b7c4c7bdae24b3b44e12bb74be701df74a4
huhn511 <sehe89@gmail.com>
2020-01-17 20:21:06 +0100

    add pdf button

--- src/renderer/views/orders/show.vue
+++ src/renderer/views/orders/show.vue
@@ -1,12 +1,15 @@
 <template>
     <div>
       <h2>Order data</h2>
+      <el-button @click="generatePDF">Generate PDF</el-button>
       <pre>{{order}}</pre>
     </div>
 </template>
 
 <script>
 import { getOrder } from '@/api/orders'
+import logo from '@/assets/logo.png'
+import { jsPDF as PDF } from 'jspdf'
 
 export default {
   name: 'OrderDetails',
@@ -14,7 +17,9 @@ export default {
   params: ['id'],
   data() {
     return {
-      order: {}
+      order: {},
+      listLoading: true,
+      logo: logo
     }
   },
   created() {
@@ -27,7 +32,51 @@ export default {
       getOrder(this.$route.params.id, this.$store.getters.token).then(response => {
         console.log('response', response)
         this.order = response
+        this.listLoading = false
       })
+    },
+    generatePDF() {
+      if (this.listLoading) {
+        console.log('loading.... wait a little bit')
+        return
+      }
+      var fontSizes = {
+        HeadTitleFontSize: 20,
+        Head2TitleFontSize: 18,
+        TitleFontSize: 14,
+        SubTitleFontSize: 16,
+        NormalFontSize: 15,
+        SmallFontSize: 10
+      }
+
+      const pdfName = 'test'
+      var doc = new PDF()
+      doc.setFont('open-sans')
+      doc.setFontSize(fontSizes.NormalFontSize)
+      // Hänge eine neue Seite an.
+      doc.addPage()
+
+      // Anschrift
+      doc.text(`${this.order.first_name} ${this.order.last_name}`, 20, 56)
+      doc.text(`${this.order.address}`, 20, 62)
+      doc.text(`${this.order.zip_code} ${this.order.city}`, 20, 68)
+      doc.text(`${this.order.country}`, 20, 74)
+
+      // einfachIOTA Logo
+      var img = new Image()
+      img.src = './' + this.logo
+      doc.addImage(img, 'PNG', 150, 40, 25, 25)
+
+      // Anschreiben Text
+      doc.text(`Hallo ${this.order.first_name},`, 20, 120)
+      doc.text(`vielen Dank, dass Du das einfachIOTA Magazin bestellt hast!`, 20, 136)
+      doc.text(`Wir wünschen Dir viel Spaß beim Lesen,`, 20, 142)
+      doc.text(`Dein einfachIOTA Team.`, 20, 154)
+
+      doc.setFontSize(fontSizes.SmallFontSize)
+      doc.text(`Wir erklären IOTA einfach - www.einfachIOTA.de`, 20, 280)
+      // Save rhe PDF
+      doc.save(pdfName + '.pdf')
     }
   }
 }
