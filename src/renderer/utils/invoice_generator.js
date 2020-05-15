e4be018f4dd0565edbc80efdded5949179f490c0
huhn511 <sehe89@gmail.com>
2020-05-15 11:40:18 +0200

    refactored invoices

---
+++ src/renderer/utils/invoice_generator.js
@@ -0,0 +1,54 @@
+var fontSizes = {
+  HeadTitleFontSize: 20,
+  Head2TitleFontSize: 18,
+  TitleFontSize: 14,
+  SubTitleFontSize: 16,
+  NormalFontSize: 15,
+  SmallFontSize: 10
+}
+
+export function createInvoice(doc, invoice, language) {
+  doc.setFontSize(fontSizes.NormalFontSize)
+  // Hänge eine neue Seite an.
+  doc.addPage()
+
+  // Anschrift
+  doc.text(`${invoice.first_name} ${invoice.last_name}`, 20, 56)
+  doc.text(`${invoice.address}`, 20, 62)
+  doc.text(`${invoice.zip_code} ${invoice.city}`, 20, 68)
+  doc.text(`${invoice.country}`, 20, 74)
+
+  // einfachIOTA Logo
+  var img = new Image()
+  img.src = './' + require('@/assets/logo.png')
+  doc.addImage(img, 'PNG', 150, 40, 25, 25)
+
+  // Anschreiben Text - DE
+  // doc.text(`Hallo ${invoice.first_name},`, 20, 120)
+  // doc.text(`vielen Dank, dass Du das einfachIOTA Magazin bestellt hast!`, 20, 136)
+  // doc.text(`Wir wünschen Dir viel Spaß beim Lesen,`, 20, 142)
+  // doc.text(`Dein einfachIOTA Team.`, 20, 154)
+
+  // Anschreiben Text - EN
+  if (language === 'DE') {
+    doc.text(`Hallo ${invoice.first_name},`, 20, 120)
+    doc.text(`vielen Dank, dass Du das einfachIOTA Magazin bestellt hast!`, 20, 136)
+    doc.text(`Wir wünschen Dir viel Spaß beim Lesen,`, 20, 142)
+    doc.text(`Dein einfachIOTA Team.`, 20, 154)
+  } else {
+    doc.text(`Hello ${invoice.first_name},`, 20, 120)
+    doc.text(`Thank you for ordering the einfachIOTA magazine!`, 20, 136)
+    doc.text(`We hope you enjoy reading it,`, 20, 142)
+    doc.text(`Your einfachIOTA Team.`, 20, 154)
+  }
+
+  doc.setFontSize(fontSizes.SmallFontSize)
+
+  if (language === 'DE') {
+    doc.text(`Wir erklären IOTA einfach - www.einfachIOTA.com`, 20, 280)
+  } else {
+    doc.text(`IOTA simple and easy - www.einfachIOTA.com`, 20, 280)
+  }
+
+  return doc
+}
--- src/renderer/views/invoices/index.vue
+++ src/renderer/views/invoices/index.vue
@@ -9,6 +9,7 @@
 <script>
 import { getList } from '@/api/orders'
 import { sendOrders } from '@/api/orders'
+import { createInvoice } from '@/utils/invoice_generator'
 import logo from '@/assets/logo.png'
 import { jsPDF as PDF } from 'jspdf'
 
@@ -35,14 +36,6 @@ export default {
       this.listLoading = true
       sendOrders(this.listQuery, this.$store.getters.token).then(response => {
         this.list = response
-        var fontSizes = {
-          HeadTitleFontSize: 20,
-          Head2TitleFontSize: 18,
-          TitleFontSize: 14,
-          SubTitleFontSize: 16,
-          NormalFontSize: 15,
-          SmallFontSize: 10
-        }
 
         const pdfName = 'test'
         var doc = new PDF()
@@ -50,41 +43,9 @@ export default {
 
         // Laufe durch alle Reechnungen durch
         this.list.forEach(invoice => {
-
           let count = invoice.amount || 1
           while (count) {
-            doc.setFontSize(fontSizes.NormalFontSize)
-            // Hänge eine neue Seite an.
-            doc.addPage()
-
-            // Anschrift
-            doc.text(`${invoice.first_name} ${invoice.last_name}`, 20, 56)
-            doc.text(`${invoice.address}`, 20, 62)
-            doc.text(`${invoice.zip_code} ${invoice.city}`, 20, 68)
-            doc.text(`${invoice.country}`, 20, 74)
-
-            // einfachIOTA Logo
-            var img = new Image()
-            img.src = './' + this.logo
-            doc.addImage(img, 'PNG', 150, 40, 25, 25)
-
-            // Anschreiben Text - DE
-            // doc.text(`Hallo ${invoice.first_name},`, 20, 120)
-            // doc.text(`vielen Dank, dass Du das einfachIOTA Magazin bestellt hast!`, 20, 136)
-            // doc.text(`Wir wünschen Dir viel Spaß beim Lesen,`, 20, 142)
-            // doc.text(`Dein einfachIOTA Team.`, 20, 154)
-
-            // Anschreiben Text - EN
-            doc.text(`Hello ${invoice.first_name},`, 20, 120)
-            doc.text(`Thank you for ordering the einfachIOTA magazine!`, 20, 136)
-            doc.text(`We hope you enjoy reading it,`, 20, 142)
-            doc.text(`Your einfachIOTA Team.`, 20, 154)
-
-            doc.setFontSize(fontSizes.SmallFontSize)
-            // DE
-            // doc.text(`Wir erklären IOTA einfach - www.einfachIOTA.com`, 20, 280)
-            // EN
-            doc.text(`IOTA simple and easy - www.einfachIOTA.com`, 20, 280)
+            doc = createInvoice(doc, invoice, null)
 
             count = count - 1
           }
