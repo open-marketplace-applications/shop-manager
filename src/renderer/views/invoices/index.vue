0992dcac0e0ca6a82ca706f57f491863ab0ff9da
huhn511 <sehe89@gmail.com>
2020-05-15 11:17:02 +0200

    add EN description

--- src/renderer/views/invoices/index.vue
+++ src/renderer/views/invoices/index.vue
@@ -32,13 +32,9 @@ export default {
       })
     },
     send() {
-      console.log('send')
       this.listLoading = true
       sendOrders(this.listQuery, this.$store.getters.token).then(response => {
         this.list = response
-
-        console.log('length', this.list.length)
-
         var fontSizes = {
           HeadTitleFontSize: 20,
           Head2TitleFontSize: 18,
@@ -54,6 +50,7 @@ export default {
 
         // Laufe durch alle Reechnungen durch
         this.list.forEach(invoice => {
+
           let count = invoice.amount || 1
           while (count) {
             doc.setFontSize(fontSizes.NormalFontSize)
@@ -71,14 +68,24 @@ export default {
             img.src = './' + this.logo
             doc.addImage(img, 'PNG', 150, 40, 25, 25)
 
-            // Anschreiben Text
-            doc.text(`Hallo ${invoice.first_name},`, 20, 120)
-            doc.text(`vielen Dank, dass Du das einfachIOTA Magazin bestellt hast!`, 20, 136)
-            doc.text(`Wir wünschen Dir viel Spaß beim Lesen,`, 20, 142)
-            doc.text(`Dein einfachIOTA Team.`, 20, 154)
+            // Anschreiben Text - DE
+            // doc.text(`Hallo ${invoice.first_name},`, 20, 120)
+            // doc.text(`vielen Dank, dass Du das einfachIOTA Magazin bestellt hast!`, 20, 136)
+            // doc.text(`Wir wünschen Dir viel Spaß beim Lesen,`, 20, 142)
+            // doc.text(`Dein einfachIOTA Team.`, 20, 154)
+
+            // Anschreiben Text - EN
+            doc.text(`Hello ${invoice.first_name},`, 20, 120)
+            doc.text(`Thank you for ordering the einfachIOTA magazine!`, 20, 136)
+            doc.text(`We hope you enjoy reading it,`, 20, 142)
+            doc.text(`Your einfachIOTA Team.`, 20, 154)
 
             doc.setFontSize(fontSizes.SmallFontSize)
-            doc.text(`Wir erklären IOTA einfach - www.einfachIOTA.de`, 20, 280)
+            // DE
+            // doc.text(`Wir erklären IOTA einfach - www.einfachIOTA.com`, 20, 280)
+            // EN
+            doc.text(`IOTA simple and easy - www.einfachIOTA.com`, 20, 280)
+
             count = count - 1
           }
         })
