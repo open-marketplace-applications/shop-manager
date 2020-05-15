efc935d188b834390751a554a3068d9decd833dd
huhn511 <sehe89@gmail.com>
2020-05-15 11:40:33 +0200

    add form to create invoice without payment

--- src/renderer/views/form/index.vue
+++ src/renderer/views/form/index.vue
@@ -1,43 +1,23 @@
 <template>
   <div class="app-container">
     <el-form ref="form" :model="form" label-width="120px">
-      <el-form-item label="Activity name">
-        <el-input v-model="form.name"></el-input>
+      <el-form-item label="first_name">
+        <el-input v-model="form.first_name"></el-input>
       </el-form-item>
-      <el-form-item label="Activity zone">
-        <el-select v-model="form.region" placeholder="please select your zone">
-          <el-option label="Zone one" value="shanghai"></el-option>
-          <el-option label="Zone two" value="beijing"></el-option>
-        </el-select>
+      <el-form-item label="last_name">
+        <el-input v-model="form.last_name"></el-input>
       </el-form-item>
-      <el-form-item label="Activity time">
-        <el-col :span="11">
-          <el-date-picker type="date" placeholder="Pick a date" v-model="form.date1" style="width: 100%;"></el-date-picker>
-        </el-col>
-        <el-col class="line" :span="2">-</el-col>
-        <el-col :span="11">
-          <el-time-picker type="fixed-time" placeholder="Pick a time" v-model="form.date2" style="width: 100%;"></el-time-picker>
-        </el-col>
+      <el-form-item label="address">
+        <el-input v-model="form.address"></el-input>
       </el-form-item>
-      <el-form-item label="Instant delivery">
-        <el-switch v-model="form.delivery"></el-switch>
+      <el-form-item label="zip_code">
+        <el-input v-model="form.zip_code"></el-input>
       </el-form-item>
-      <el-form-item label="Activity type">
-        <el-checkbox-group v-model="form.type">
-          <el-checkbox label="Online activities" name="type"></el-checkbox>
-          <el-checkbox label="Promotion activities" name="type"></el-checkbox>
-          <el-checkbox label="Offline activities" name="type"></el-checkbox>
-          <el-checkbox label="Simple brand exposure" name="type"></el-checkbox>
-        </el-checkbox-group>
+      <el-form-item label="city">
+        <el-input v-model="form.city"></el-input>
       </el-form-item>
-      <el-form-item label="Resources">
-        <el-radio-group v-model="form.resource">
-          <el-radio label="Sponsor"></el-radio>
-          <el-radio label="Venue"></el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="Activity form">
-        <el-input type="textarea" v-model="form.desc"></el-input>
+      <el-form-item label="country">
+        <el-input v-model="form.country"></el-input>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="onSubmit">Create</el-button>
@@ -48,24 +28,32 @@
 </template>
 
 <script>
+import { createInvoice } from '@/utils/invoice_generator'
+import { jsPDF as PDF } from 'jspdf'
+
 export default {
   data() {
     return {
       form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
+        first_name: '',
+        last_name: '',
+        address: '',
+        zip_code: '',
+        city: '',
+        country: ''
       }
     }
   },
   methods: {
     onSubmit() {
       this.$message('submit!')
+      const pdfName = 'test'
+      var doc = new PDF()
+      doc.setFont('open-sans')
+      doc = createInvoice(doc, this.form, 'DE')
+
+      // Save rhe PDF
+      doc.save(pdfName + '.pdf')
     },
     onCancel() {
       this.$message({
@@ -78,7 +66,7 @@ export default {
 </script>
 
 <style scoped>
-.line{
+.line {
   text-align: center;
 }
 </style>
